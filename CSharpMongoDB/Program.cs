3f18ddf348dc6ffa017211a5e68c6829c8b4350c
DevelopHack <lionel2688@gmail.com>
2017-06-27 13:48:11 -0500

    6-27-2017

--- CSharpMongoDB/Program.cs
+++ CSharpMongoDB/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 namespace CSharpMongoDB
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the address of the mongodb server, for example: mongodb://localhost:27017");
+            string stringConnection = Console.ReadLine();
+            try
+            {
+                MongoClient client = new MongoClient(stringConnection);
+                List<BsonDocument> listDatabase = client.ListDatabases().ToList();
+               
+                Console.WriteLine("***List of exiting Database***");
+                foreach (var item in listDatabase)
+                {
+                    Console.WriteLine(item["name"]);
+                }
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect address");
+            }
+           
         }
     }
 }
